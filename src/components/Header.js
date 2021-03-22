3e72f540fbc68fb50b1efb0da28083f73a422cc0
chadidi02 <souhaibchadidi02@gmail.com>
2021-03-22 18:33:01 +0100

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+export default function Nav() {
+	return (
+		<header className="App-header">
+			<nav id="header" className="w-full z-30 top-0 py-1 bg-white">
+				hi
+			</nav>
+		</header>
+	);
+}
